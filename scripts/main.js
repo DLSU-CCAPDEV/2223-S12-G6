ac2037604d00d1cf8e5954053bf2840be99d80b1
Christian Ibaoc <christian_ibaoc@dlsu.edu.ph>
2023-06-18 14:02:00 +0800

    base works, need designs

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,36 @@
-/*window.onload = function()
+const revDivNode = document.createElement("div");
+revDivNode.className="review";
+revDivNode.textContent="INSERT REVIEW/S HERE";
+
+window.onload = function()
 {
-    document.getElementById("reviews").style.display="none";
-    document.getElementById("store").style.height="90%";
+    var buttons = document.getElementsByClassName("storename");
+    var stores = document.getElementsByClassName("store");
+    for(x=0;x<=stores.length;x++)
+    {
+        stores[x].id = "store"+x;
+        buttons[x].id = "button"+x;
+        buttons[x].onclick = function disp()
+        {   
+            storeClick(this.id);
+        }
+    }
+    
 }
-*/
 function test()
 {
     document.getElementById("test").innerHTML="clicked";
     document.getElementById("button").innerHTML="Clickewd";
 }
 
-function storeClick()
+function storeClick(id)
 {
+    me = document.getElementById(id);
+    store = me.parentNode;
+    if(![...store.children].includes(revDivNode))
+            store.append(revDivNode);
+    else
+            store.removeChild(revDivNode);
 
     var revs = document.getElementsByClassName("review");
     for(i=0;i<revs.length;i++)
