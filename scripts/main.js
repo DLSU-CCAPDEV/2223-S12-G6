ecf9216721c3c0a9e42a8d6a182a1dc5ff2f31e9
Christian Ibaoc <christian_ibaoc@dlsu.edu.ph>
2023-06-18 12:35:01 +0800

    added changes, removed more stores sa right main page will not display all stores

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,24 @@
+/*window.onload = function()
+{
+    document.getElementById("reviews").style.display="none";
+    document.getElementById("store").style.height="90%";
+}
+*/
 function test()
 {
     document.getElementById("test").innerHTML="clicked";
     document.getElementById("button").innerHTML="Clickewd";
+}
+
+function storeClick()
+{
+
+    var revs = document.getElementsByClassName("review");
+    for(i=0;i<revs.length;i++)
+    {
+        revs[i].onclick = function review()
+        {
+            this.innerHTML="Banana";
+        }
+    }
 }
