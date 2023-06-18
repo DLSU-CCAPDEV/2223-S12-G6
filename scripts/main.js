c1bf135e34da8bc118cff908e9202335aae523f6
Christian Ibaoc <christian_ibaoc@dlsu.edu.ph>
2023-06-18 17:31:52 +0800

    Improved(?) Designs

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,14 @@
 const revDivNode = document.createElement("div");
 revDivNode.className="review";
 revDivNode.textContent="INSERT REVIEW/S HERE";
+revDivNode.hidden=false;
 
 window.onload = function()
 {
+    //Gets all the store divs and store buttons, as in the future
+    // we will have multiple stores. Also adds an id to them automatically
+    // upon loading. Personally makes it easier but if theres another way
+    // go ahead lang.
     var buttons = document.getElementsByClassName("storename");
     var stores = document.getElementsByClassName("store");
     for(x=0;x<=stores.length;x++)
@@ -18,21 +23,39 @@ window.onload = function()
     }
     
 }
-function test()
-{
-    document.getElementById("test").innerHTML="clicked";
-    document.getElementById("button").innerHTML="Clickewd";
-}
 
+/*
+Function for what happens on click of a store's name
+*/
 function storeClick(id, x)
 {
-    me = document.getElementById(id);
-    store = me.parentNode;
-    if(![...store.children].includes(revDivNode))
-            store.append(revDivNode);
-    else
-            store.removeChild(revDivNode);
+    var me = document.getElementById(id);
+    var store = me.parentNode;
+    var storeid = store.id;
+    var stores = document.getElementsByClassName("store");
 
+    //Ensures only one store is expanded at one time
+    for(x =0;x<stores.length;x++)
+    {
+        stores[x].style.height="100px";
+    }
+
+    //Appends the Review Div Node to the clicked store.
+    //Needs functionailty that the reviews change for each store.
+    if(![...store.children].includes(revDivNode))
+    {
+        document.getElementById(storeid).style.height="400px";
+        store.append(revDivNode);
+    }
+    else
+    {
+        document.getElementById(storeid).style.height="100px";
+        store.removeChild(revDivNode);
+    }
+
+    //Placeholder functionally for onlick events for reviews,
+    // ie, clicking the review will do something, in this case
+    // it will turn the text into "Banana"
     var revs = document.getElementsByClassName("review");
     for(i=0;i<revs.length;i++)
     {
