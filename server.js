6877198803ea1c5f5e153f66e626a3c3e7507584
Christian Ibaoc <christian_ibaoc@dlsu.edu.ph>
2023-08-02 15:39:34 +0800

    changed to http

--- server.js
+++ server.js
@@ -1,35 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const mongodb = require('mongodb');
-const fs = require('fs');
 const bodyParser =  require("body-parser");
-const controller = require("./controllers/controller.js");
-const { error } = require('console');
 const hbs = require('hbs');
-const { randomInt } = require('crypto');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
-loggedin = false;
-uname = null;
-avatar = null;
 const routes = require('./routes/routes.js');
 const url = "mongodb+srv://TM6606:TM6606@bananacluster.lzoy6lc.mongodb.net/?retryWrites=true&w=majority";
+const http = require('http');
+const https = require('https');
 //const url = "mongodb://127.0.0.1:27017/";
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:false}));
-const client = new mongodb.MongoClient(url, {
-  serverApi:
-  {
-    version:mongodb.ServerApiVersion.v1,
-    strict:true,
-    deprecationErrors: true
-  },
-  useUnifiedTopology : true
-});
-const options = {
-  projection: {_id :0, name:1,pass:1},  
-}
 
 dotenv.config();
 var port = process.env.PORT;
@@ -53,10 +36,17 @@ app.use(session({
 
 app.use('/', routes);
 
+/*
 app.listen(port,hostname,function()
 {
-  console.log("Server listening at http://" + hostname + ":" +port );
+  
 })
+*/
+http.createServer(app).listen(port,hostname, ()=>
+{
+  console.log("Running Server at " + hostname+ ':' + port);
+});
+
 
 hbs.registerHelper('ifEquals', function(var1,var2,options)
 {
