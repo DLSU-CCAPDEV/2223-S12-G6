9729254916d40ff1f9262ef4f07b516b1850330c
Christian Ibaoc <christian_ibaoc@dlsu.edu.ph>
2023-08-03 10:13:02 +0800

    tried http again

--- server.js
+++ server.js
@@ -5,13 +5,16 @@ const hbs = require('hbs');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const routes = require('./routes/routes.js');
+const http = require('http') , https = require('https');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:false}));
 
 dotenv.config();
-var port = process.env.PORT;
-var hostname  = process.env.HOSTNAME;
+const PORT = process.env.PORT;
+const PORT2 = 3443;
+const HOSTNAME  = process.env.HOSTNAME;
 
 app.set('view engine', 'hbs');
 app.use(express.static('public'));
@@ -31,13 +34,26 @@ app.use(session({
 
 app.use('/', routes);
 
-
-app.listen(port,hostname,function()
+/*
+app.listen(PORT,HOSTNAME,function()
 {
   console.log("Running Server at " + hostname+ ':' + port);
 })
+*/
 
 
+
+http.createServer(app).listen(PORT); 
+https.createServer(app).listen(PORT2, ()=>
+{
+  console.log("Server is running at port " + PORT);
+});
+
+app.get('/',function(req,res)
+{
+  console.log('req');
+  res.send('wassap');
+})
 
 hbs.registerHelper('ifEquals', function(var1,var2,options)
 {
